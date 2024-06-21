57f5da8ce6ccf34bc6002ab9667de977836438a0
monish <monish2304@gmail.com>
2024-06-21 16:54:02 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+       <div className='  text-center text-lg text-red-700'>Hello World</div>
+    </div>
+    
+    
+  )
+}
+
+export default App
